9c11af37167884cd156f044561b85f136f6e87a2
rdigital <tushar.madiya@capgemini.com>
2015-12-02 17:26:07 +0000

    1st time check-in

---
+++ SampleMVCApplication.Tests/Controllers/MapsControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using SampleMVCApplication;
+using SampleMVCApplication.Controllers;
+
+namespace SampleMVCApplication.Tests.Controllers
+{
+    [TestClass]
+    public class MapsControllerTest
+    {
+        [TestMethod]
+        public void ViewMaps()
+        {
+            // Arrange
+            MapsController controller = new MapsController();
+
+            // Act
+            ViewResult result = controller.ViewMaps() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ SampleMVCApplication/Controllers/MapsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleMVCApplication.Controllers
+{
+    public class MapsController : Controller
+    {
+        // GET: Maps
+        public ActionResult Index()
+        {
+            // Add action logic here
+            throw new NotImplementedException();
+        }
+
+        public ActionResult ViewMaps()
+        {
+            return View();
+        }
+    }
+}
